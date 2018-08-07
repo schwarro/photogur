2d925994882377de5227aed599aadbe8b7e984b5
schwarro <rob.ap.schwartz@gmail.com>
2018-08-07 16:16:37 -0400

    completed delete picture section of exercise

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -45,4 +45,10 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end 
 end
