d906c52286c67516f92d5338a6a1336938bac9c0
schwarro <rob.ap.schwartz@gmail.com>
2018-08-13 16:15:44 -0400

    completed validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,9 @@
 class Picture < ApplicationRecord
 
+  validates :title, length: {within: 3..20}
+  validates :artist, :url, presence: true 
+  validates :url, uniqueness: true
+
   def self.created_before(time)
     Picture.where("created_at < ?", time)
   end
