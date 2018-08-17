51340032d34ab4edc183b5685219d3a234994d5c
schwarro <rob.ap.schwartz@gmail.com>
2018-08-17 15:58:02 -0400

    completed authorizing a picture's owner section

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,8 @@
 class PicturesController < ApplicationController
 
   before_action :ensure_logged_in, except: [:show, :index]
+  before_action :load_picture, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
 
   def index
     @pictures = Picture.all
@@ -10,7 +12,7 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
+    load_picture
   end
 
   def new
@@ -27,7 +29,8 @@ class PicturesController < ApplicationController
 
     if @picture.save
       # if the picture gets saved, generate a get request to "/pictures" (the index)
-      redirect_to "/pictures"
+      # redirect_to "/pictures"
+      redirect_to pictures_url, notice: "Picture Added"
     else
       # otherwise render new.html.erb
       render :new
@@ -35,11 +38,11 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(params[:id])
+    load_picture
   end
 
   def update
-    @picture = Picture.find(params[:id])
+    load_picture
 
     @picture.title = params[:picture][:title]
     @picture.artist = params[:picture][:artist]
@@ -47,17 +50,31 @@ class PicturesController < ApplicationController
 
 
     if @picture.save
-      redirect_to "/pictures/#{@picture.id}"
+      # redirect_to "/pictures/#{@picture.id}"
+      redirect_to picture_url(@picture.id)
     else
       render :edit
     end
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
+    load_picture
     @picture.destroy
     redirect_to "/pictures"
   end
+
+  def ensure_user_owns_picture
+    unless current_user == @picture.user
+      flash[:alert] = "Please log in"
+      redirect_to new_session_url
+    end
+  end
+
+  def load_picture
+    @picture = Picture.find(params[:id])
+  end
+
+
 
 
 end
